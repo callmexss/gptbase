0791127a27220adaff88d4d9e827f3949c4f0b50
callmexss <callmexss@126.com>
2023-05-28 17:51:11 +0800

    feat: add feature for calculate tokens.

--- gptbase/basev2.py
+++ gptbase/basev2.py
@@ -3,6 +3,7 @@ from collections import deque
 
 import openai
 import rich
+import tiktoken
 from rich.logging import RichHandler
 
 
@@ -138,6 +139,57 @@ class ChatAssistant(Assistant):
         return message
 
 
+# Constants for token counts and model encodings
+TOKENS_PER_MESSAGE = {"gpt-3.5-turbo-0301": 4, "gpt-4-0314": 3}
+TOKENS_PER_NAME = {"gpt-3.5-turbo-0301": -1, "gpt-4-0314": 1}
+DEFAULT_MODEL = "cl100k_base"
+WARNINGS = {
+    "gpt-3.5-turbo": "Warning: gpt-3.5-turbo may change over time. Returning num tokens assuming gpt-3.5-turbo-0301.",
+    "gpt-4": "Warning: gpt-4 may change over time. Returning num tokens assuming gpt-4-0314.",
+}
+
+
+def get_encoding(model):
+    """Get the encoding for a model, with a fallback to the default model if not found."""
+    try:
+        return tiktoken.encoding_for_model(model)
+    except KeyError:
+        print(f"Warning: model {model} not found. Using {DEFAULT_MODEL} encoding.")
+        return tiktoken.get_encoding(DEFAULT_MODEL)
+
+
+def count_tokens_in_message(message, encoding, model):
+    """Count the tokens in a single message."""
+    tokens = TOKENS_PER_MESSAGE.get(model, 0)
+    for key, value in message.items():
+        tokens += len(encoding.encode(value))
+        if key == "name":
+            tokens += TOKENS_PER_NAME.get(model, 0)
+    return tokens
+
+
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301"):
+    """Returns the number of tokens used by a list of messages."""
+    if model in WARNINGS:
+        print(WARNINGS[model])
+        return num_tokens_from_messages(messages, model=f"{model}-0314")
+    if model not in TOKENS_PER_MESSAGE:
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
+        )
+
+    encoding = get_encoding(model)
+    num_tokens = 0
+
+    if isinstance(messages, list):
+        for message in messages:
+            num_tokens += count_tokens_in_message(message, encoding, model)
+        num_tokens += 3  # every reply is primed with assistant
+    elif isinstance(messages, str):
+        num_tokens += len(encoding.encode(messages))
+    return num_tokens
+
+
 if __name__ == "__main__":
     cm = CompletionParameters()
 
@@ -145,6 +197,15 @@ if __name__ == "__main__":
     response = assistant.ask("What is the weather today?")
     message = response.choices[0].message.content
     print(message)
+
+    cm.stream = True
+    response = assistant.ask("How to learn python?", params=cm)
+    li = []
+    for chunk in get_chunks(response):
+        li.append(chunk)
+        print(chunk, end="")
+    print()
+    num_tokens_from_messages("".join(li))
 
     assistant = ChatAssistant()
     cm.stream = True
